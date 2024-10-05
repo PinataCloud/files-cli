d4e1c3b0fde209455bd1d0db9cdccb99904b71c2
Steve <stevedsimkins@gmail.com>
2024-10-04 21:55:07 -0400

    chore: Moved findToken to auth

--- auth.go
+++ auth.go
@@ -46,6 +46,23 @@ func SaveJWT(jwt string) error {
 	return nil
 }
 
+func findToken() ([]byte, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	dotFilePath := filepath.Join(homeDir, ".pinata-files-cli")
+	JWT, err := os.ReadFile(dotFilePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, errors.New("JWT not found. Please authorize first using the 'auth' command")
+		} else {
+			return nil, err
+		}
+	}
+	return JWT, err
+}
+
 func GetHost() string {
 	return GetEnv("PINATA_HOST", "api.pinata.cloud")
 }
--- uploads.go
+++ uploads.go
@@ -97,23 +97,6 @@ func Upload(filePath string, groupId string, name string, cidOnly bool) (UploadR
 	return response, nil
 }
 
-func findToken() ([]byte, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return nil, err
-	}
-	dotFilePath := filepath.Join(homeDir, ".pinata-go-cli")
-	JWT, err := os.ReadFile(dotFilePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, errors.New("JWT not found. Please authorize first using the 'auth' command")
-		} else {
-			return nil, err
-		}
-	}
-	return JWT, err
-}
-
 type progressReader struct {
 	r   io.Reader
 	bar *progressbar.ProgressBar
